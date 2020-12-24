cf3cf40346ba2bdd7886bbbf9e82233c9f454875
Dan Boots <thestereojackers@gmail.com>
2020-12-24 07:14:24 +0000

    option to add genres to mongo Db

--- app.py
+++ app.py
@@ -145,6 +145,25 @@ def delete_review(review_id):
     return redirect(url_for("get_reviews"))
 
 
+@app.route("/get_genre")
+def get_genres():
+    genres = list(mongo.db.genre.find().sort("genre_name", 1))
+    return render_template("genres.html", genres=genres)
+
+
+@app.route("/add_genre", methods=["GET", "POST"])
+def add_genre():
+    if request.method == "POST":
+        genre = {
+            "genre_name": request.form.get("genre_name")
+        }
+        mongo.db.genre.insert_one(genre)
+        flash("New Genre Added")
+        return redirect(url_for("get_genres"))
+
+    return render_template("add_genre.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
